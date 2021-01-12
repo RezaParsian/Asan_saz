9acb2e633fb2f074772a60e8756c6211b443f642
Reza_Atom <info@rp76.ir>
2021-01-12 23:19:15 +0330

    nullable some filed

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password')->nullable();
             $table->string("code_meli",20)->nullable();
             $table->timestamp("birth")->nullable();
             $table->string("last_version",20)->nullable();
