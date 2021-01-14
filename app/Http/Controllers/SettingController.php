c45aabfa7e39dafa26688b8e9f3aa79701f40c03
Reza_Atom <info@rp76.ir>
2021-01-14 16:53:08 +0330

    make new api for sohrab

--- app/Http/Controllers/SettingController.php
+++ app/Http/Controllers/SettingController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Setting;
 use Illuminate\Http\Request;
 
 class SettingController extends Controller
 {
-    //
+    public function Fetch()
+    {
+        $setting=Setting::get();
+        return $setting;
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\SettingController;
 use App\Http\Controllers\SmsController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -21,3 +24,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 Route::post('/SendSms', [SmsController::class,"SendSms"])->name("api-sendsms");
 Route::post('/MakeUser', [SmsController::class,"MakeUser"])->name("api-makeuser");
+
+Route::post('/Setting', [SettingController::class,"Fetch"])->name("api-fetchsetting");
+Route::post('/Product', [ProductController::class,"Fetch"])->name("api-fetchsetting");
+Route::get('/Category', [CategoryController::class,"Fetch"])->name("api-fetchsetting");
