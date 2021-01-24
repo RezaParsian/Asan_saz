cf5a78009298b99e06d54b0654b1cc7979a18471
Reza_Atom <info@rp76.ir>
2021-01-24 19:41:52 +0330

    add address stuff

---
+++ app/Http/Controllers/AddressController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\address;
+use Illuminate\Http\Request;
+
+class AddressController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\address  $address
+     * @return \Illuminate\Http\Response
+     */
+    public function show(address $address)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\address  $address
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, address $address)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\address  $address
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(address $address)
+    {
+        //
+    }
+}
